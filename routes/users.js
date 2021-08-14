39c13aeb2881fafe4d95c8a4287fc0aa6f634265
athenas2021 <lucas.kano@outlook.com>
2021-08-14 17:14:12 -0300

    Adicionando validadores de mensagens de erro

--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,37 @@ router.get('/login', (req, res) => res.render('login'));
 //Pagina registro
 router.get('/register', (req, res) => res.render('register'));
 
+// Registrador
+router.post('/register',(req,res) => {
+    const {name,email,password, password2} = req.body;
+    let errors = [];
+
+    //checar campos obrigatorios
+    if(!name || !email || !password || !password2 ){
+        errors.push({msg: 'Favor preencher todos os campos'});
+    }
+
+    //Checar se as senhas sao iguais
+    if(password !== password2){
+        errors.push({msg:'As senhas nao sao iguais'});
+    }
+
+    //Checar tamanho
+    if(password.length < 6){
+        errors.push({msg:'A senha deve conter no minimo 6 caracteres'});
+    }
+
+    if (errors.length > 0){
+        res.render('register',{
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    }else{
+        res.send('passou')
+    }
+});
+
 module.exports = router;
