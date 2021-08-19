b5567fe2b48dca028db26b87eb4a0278d0a837d4
athenas2021 <lucas.kano@outlook.com>
2021-08-19 08:57:20 -0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
-router.get('/', (req, res) => res.render('index'))
+//Pagina index
+router.get('/', (req, res) => res.render('index'));
+
+//Pagina Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    user: req.user
+  })
+);
 
 module.exports = router;
